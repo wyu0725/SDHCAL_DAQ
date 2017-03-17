786474525dc73b28bcbc24f29e9122bd6040a845
Yu Wang <wyu0831@hotmail.com>
2017-03-17 22:49:16 +0800

    增加读取DAC码值模块

--- Matlab/SCurveTest/ReadData.m
+++ Matlab/SCurveTest/ReadData.m
@@ -1,4 +1,4 @@
-function [Channel, DAC_Code, CountP0, CountT0, CountP1, CountT1, CountP2, CountT2] = ReadDAC(Initialdata, ReadStart)
+function [Channel, DAC_Code, CountP0, CountT0, CountP1, CountT1, CountP2, CountT2] = ReadData(Initialdata, ReadStart)
 Channel = Initialdata(ReadStart);
 DAC_Code = 1:1:1024;
 CountP0 = 1:1:1024;
@@ -8,7 +8,7 @@ CountT0 = 1:1:1024;
 CountT1 = 1:1:1024;
 CountT2 = 1:1:1024;
 for i = 1:1:1024
-    DAC_Code = Initialdata(ReadStart + 7*(i-1) + 1);
+    DAC_Code(i) = bitand((Initialdata(ReadStart + 7*(i-1) + 1)), uint16(1023));
     CountP0(i) = Initialdata(ReadStart + 7*(i-1) + 2);
     CountT0(i) = Initialdata(ReadStart + 7*(i-1) + 3);
     CountP1(i) = Initialdata(ReadStart + 7*(i-1) + 4);
--- Matlab/SCurveTest/SCurvePlot.m
+++ Matlab/SCurveTest/SCurvePlot.m
@@ -1,7 +1,7 @@
 %% SCurve Plot
 InitialData = Importdata();
 Test_Header = InitialData(1);
-[Test_Channel, DAC_Code, P0, T0, P1, T1, P2, T2] = ReadDAC(InitialData, 2);
+[Test_Channel, DAC_Code, P0, T0, P1, T1, P2, T2] = ReadData(InitialData, 2);
 DAC_Code1 = 1:1:1024;
 Trig_Ratio0 = T0./P0;
 Trig_Ratio1 = T1./P1;
@@ -16,15 +16,21 @@ x1_E = End0 + 10;
 x2_S = Start2 - 10;
 x2_E = End2 + 10;
 Legend_Trig0 = sprintf('Trigger0:Trigger Rate vs DAC Code\n Middle of trigger rate is :%f', Middle0);
+Legend_Trig1 = sprintf('Trigger1:Trigger Rate vs DAC Code\n Middle of trigger rate is :%f', Middle1);
+Legend_Trig2 = sprintf('Trigger2:Trigger Rate vs DAC Code\n Middle of trigger rate is :%f', Middle2);
 
 figure(1)
-plot(DAC_Code1, Trig_Ratio0);
+plot(DAC_Code, Trig_Ratio0);
 axis([x0_S x0_E, -0.2 1.2])
 h = legend(Legend_Trig0);
 set(h,'Location','northoutside');
 figure(2)
-plot(DAC_Code1, Trig_Ratio1);
+plot(DAC_Code, Trig_Ratio1);
 axis([x1_S x1_E, -0.2 1.2])
+h = legend(Legend_Trig1);
+set(h,'Location','northoutside');
 figure(3)
-plot(DAC_Code1, Trig_Ratio2);
+plot(DAC_Code, Trig_Ratio2);
 axis([x2_S x2_E, -0.2 1.2])
+h = legend(Legend_Trig2);
+set(h,'Location','northoutside');
