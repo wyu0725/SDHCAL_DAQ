b63c510700ead018a24cfdb9430e577e10c4cacf
Yu Wang <wyu0831@hotmail.com>
2017-03-22 18:50:23 +0800

    保存两个图片

--- Matlab/SCurveTest/SCurvePlot_64Channel.m
+++ Matlab/SCurveTest/SCurvePlot_64Channel.m
@@ -1,13 +1,13 @@
 %% 64 Channel SCurvePlot
 InitialData = Importdata();
-legend_str = cell(1,64);
+legend_str = cell(8,8);
 for i=0:1:63
     SCurvePlotSingleChannel(InitialData, i);
-    legend_str{i+1} = sprintf('Channel%d',i+1);    
+    %legend_str{i+1} = sprintf('Channel%d',i+1);    
 end
 % legend_str = {'Channel1','Channel2'};
 figure(1)
-legend(legend_str);
+%legend(legend_str{1});
 axis([0 1023,-10 110]);
 hold off;
 figure(2)
