5bf3f8177f29b8254ecd534fcfcd84bdf8dc425d
Yu Wang <wyu0831@hotmail.com>
2017-03-31 14:03:22 +0800

    使用三次样条插值来确定50%触发率对应的DAC码值

--- Matlab/SCurveTest/FindStartMidEnd.m
+++ Matlab/SCurveTest/FindStartMidEnd.m
@@ -17,14 +17,20 @@ for i = 1:1:Len - 1
     end
 end
 temp = 100;
-x_i = Start:End;
-y_i = Trig_Ratio(Start:End);
-x_SI = Start:0.01:End;
+I_Start = Start - 2;
+I_End = End + 2;
+x_i = I_Start:I_End;
+y_i = Trig_Ratio(x_i);
+x_SI = I_Start:0.01:I_End;
 y_SI = spline(x_i,y_i,x_SI);
-n = (End - Start)/0.01 + 1;
+n = (I_End - I_Start)/0.01 + 1;
 for i = 1:1:n
-    if(abs(y_SI))
+    if(abs(y_SI(i) - Percent) < temp)
+        temp = abs(y_SI(i) - Percent);
+        Middle_temp = i;
+    end
 end
+Middle = Middle_temp*0.01 + I_Start;
 % for i = 1:1:Len - 1
 %     if(abs(Trig_Ratio(i) - Percent) < temp)
 %         temp = abs(Trig_Ratio(i) - Percent);
