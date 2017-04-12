49f5e97da2a1868b8a7eb69f60ec961d6002ddc9
Yu Wang <wyu0831@hotmail.com>
2017-04-12 15:12:07 +0800

    读取PAd映射关系

--- Matlab/PColor/GetPadMapData.m
+++ Matlab/PColor/GetPadMapData.m
@@ -1,8 +1,14 @@
 function [ MapData ] = GetPadMapData()
 %UNTITLED3 此处显示有关此函数的摘要
 %   此处显示详细说明
-    MapData = zeros(30, 30);
-    mapping = 
+    MapData = cell(30, 30);
+    [fid,~] = fopen('Pad_Order.txt','r');
+    mapping = textscan(fid, '%s');
+    for i = 1:1:30
+       for j = 1:1:30
+           MapData(i, j) = mapping{1}(j + 30*(i - 1));
+       end
+    end
 
 end
 
