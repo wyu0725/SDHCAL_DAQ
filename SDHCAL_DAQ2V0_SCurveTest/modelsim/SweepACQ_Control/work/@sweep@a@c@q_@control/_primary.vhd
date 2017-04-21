36e3f4a940944486302d95b390e63db327078fcc
Yu Wang <wyu0831@hotmail.com>
2017-04-21 14:49:52 +0800

    ModelSIM工程

---
+++ SDHCAL_DAQ2V0_SCurveTest/modelsim/SweepACQ_Control/work/@sweep@a@c@q_@control/_primary.vhd
@@ -0,0 +1,23 @@
+library verilog;
+use verilog.vl_types.all;
+entity SweepACQ_Control is
+    port(
+        Clk             : in     vl_logic;
+        reset_n         : in     vl_logic;
+        SweepStart      : in     vl_logic;
+        SingleACQStart  : out    vl_logic;
+        OneDACDone      : out    vl_logic;
+        ACQDone         : out    vl_logic;
+        StartDAC0       : in     vl_logic_vector(9 downto 0);
+        EndDAC0         : in     vl_logic_vector(9 downto 0);
+        MaxPackageNumber: in     vl_logic_vector(15 downto 0);
+        ParallelData_en : in     vl_logic;
+        OutDAC0         : out    vl_logic_vector(9 downto 0);
+        LoadSCParameter : out    vl_logic;
+        MicrorocConfigDone: in     vl_logic;
+        SweepACQFifoData: in     vl_logic_vector(15 downto 0);
+        SweepACQFifoData_rden: out    vl_logic;
+        SweepACQData    : out    vl_logic_vector(15 downto 0);
+        SweepACQData_en : out    vl_logic
+    );
+end SweepACQ_Control;
---
+++ SDHCAL_DAQ2V0_SCurveTest/modelsim/SweepACQ_Control/work/@sweep@a@c@q_@control_tb/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity SweepACQ_Control_tb is
+end SweepACQ_Control_tb;
