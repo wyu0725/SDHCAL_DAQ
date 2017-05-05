8ec757675f25546504760724f695023a50ab20d3
Yu Wang <wyu0831@hotmail.com>
2017-05-05 19:42:35 +0800

    修改处理S曲线不单调的方式

--- Matlab/SCurveTest/CaculateStd.m
+++ Matlab/SCurveTest/CaculateStd.m
@@ -2,23 +2,24 @@ function [ Dac0Std, Dac1Std, Dac2Std ] = CaculateStd( ImportData, Channel_Number
 %UNTITLED3 此处显示有关此函数的摘要
 %   此处显示详细说明
     [~, ~, P0, T0, P1, T1, P2, T2] = ReadData(ImportData, 2 + Channel_Number*7169);
-    Trig_Ratio0 = (T0./P0);
-    Trig_Ratio1 = (T1./P1);
-    Trig_Ratio2 = (T2./P2);
-    for i = 1:1:1023
-        if(Trig_Ratio0(i) > Trig_Ratio0(i+1))
-            Trig_Ratio0(i + 1) = Trig_Ratio0(i);
+    TrigRatio0 = (T0./P0);
+    TrigRatio1 = (T1./P1);
+    TrigRatio2 = (T2./P2);
+    for i = 1024:-1:2
+        if(TrigRatio0(i-1) > TrigRatio0(i))
+            TrigRatio0(i-1) = TrigRatio0(i);
         end
-        if(Trig_Ratio1(i) > Trig_Ratio1(i+1))
-            Trig_Ratio1(i + 1) = Trig_Ratio1(i);
+        if(TrigRatio1(i-1) > TrigRatio1(i))
+            TrigRatio1(i-1) = TrigRatio1(i);
         end
-        if(Trig_Ratio2(i) > Trig_Ratio2(i+1))
-            Trig_Ratio2(i + 1) = Trig_Ratio2(i);
+        if(TrigRatio2(i-1) > TrigRatio2(i))
+            TrigRatio2(i-1) = TrigRatio2(i);
         end
     end
-    DiffT0 = diff(Trig_Ratio0);
-    DiffT1 = diff(Trig_Ratio1);
-    DiffT2 = diff(Trig_Ratio2);
+
+    DiffT0 = diff(TrigRatio0);
+    DiffT1 = diff(TrigRatio1);
+    DiffT2 = diff(TrigRatio2);
 %     DacCode(1024) = [];
     DacCode = 1:1023;
     MeanDac0 = DacCode*DiffT0';
