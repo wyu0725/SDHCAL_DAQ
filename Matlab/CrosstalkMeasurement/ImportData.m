dcb176d2ba06f8d4c52ebae7017c89afcc70ee56
Yu Wang <wyu0831@hotmail.com>
2017-05-26 13:51:36 +0800

    123

---
+++ Matlab/CrosstalkMeasurement/ImportData.m
@@ -0,0 +1,20 @@
+function InitialData = ImportData()
+%%% This function inport the *.dat file with UI interface
+% The data is imported in ubit2 structure
+[FileName,PathName,FilterIndex] = uigetfile('*.dat','Select the file');
+if FilterIndex
+    filename = [PathName FileName];
+    [fid,~] = fopen(filename,'r');
+    if fid <= 0
+        % There was an error--tell user
+        str = ['File ' filename ' could not be opened.'];
+        dlg_title = 'File Open Faild';
+        errordlg(str, dlg_title,'modal');
+    else
+        %File opend successfully
+        InitialData = fread(fid,'ubit2','ieee-be');%Big-endian ording
+        %Size = length(importdata);
+        fclose(fid);%close file
+    end
+end
+end
