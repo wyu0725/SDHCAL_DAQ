7ff798fd01a776e71767568a0b106b9759badb61
Yu Wang <wyu0831@hotmail.com>
2017-06-21 19:51:20 +0800

    串扰分析程序

--- Matlab/PColor/Plot8x8Sum_22.m
+++ Matlab/PColor/Plot8x8Sum_22.m
@@ -18,93 +18,69 @@ for i = 1:DisplayNo
     ChannelDataTemp = ChannelDataTemp + Ch_data;
 end
 [MaxNumber,FiredChannel] = max(ChannelDataTemp);
+%FiredChannel=FiredChannelt-1;
 SumData = zeros(64,1);
+Sum_t = zeros(64,1);
 NotHitted = zeros(64,1);
 GreatThan1 = zeros(64,1);
 GreatThan2 = zeros(64,1);
 GreatThan3 = zeros(64,1);
 FiredCount = 0;
-t1 = 0;
-t2 = 0;
-t3 = 0;
-t11 = 0;
-t22 = 0;
-t33 = 0;
+t_count=0;
+t1=0;t2=0;t3=0;
+%f1=0;f2=0;f3=0;
+f_count=0;
+t11=zeros(DisplayNo,1);t12=zeros(DisplayNo,1);t13=zeros(DisplayNo,1);
+
 for DisplayK = 1:DisplayNo
     [header, BCID, Ch_data] = ReadPackage(InitialData, PackNo + DisplayK - 1);
 %     if(header ~= hex2dec('A0'))
 %         continue;
 %     end
 %     NewChannelData = SingleMapping(Ch_data, ASIC_Channel, Pad_Channel);
-    if(Ch_data(FiredChannel) == 3)
-        for k = 1:1:64
-            if(Ch_data(k) == 0)
-                NotHitted(k) = NotHitted(k) + 1;
-            end
-            if(Ch_data(k) == 1)
-                GreatThan1(k) = GreatThan1(k) + 1;
-            end
-            if(Ch_data(k) == 2)
-                GreatThan2(k) = GreatThan2(k) + 1;
-            end
-            if(Ch_data(k) == 3)
+   if(max(Ch_data) == Ch_data(FiredChannel) && Ch_data(FiredChannel) == 3) 
+   %  if(max(Ch_data) == Ch_data(FiredChannel)) 
+      for k = 1:1:64
+        % if(k==FiredChannel) continue;
+        if (  k==FiredChannel+1 || k==FiredChannel-8 || k==FiredChannel-1 || k==FiredChannel-7 || k==FiredChannel-9 ||  k==FiredChannel+7 || k==FiredChannel+9 || k==FiredChannel+8)
+           if(Ch_data(k) == 3  && k~=FiredChannel)
                 GreatThan3(k) = GreatThan3(k) + 1;
-            end
+                t3=t3+1;
+               
+               % k
+                 t13(DisplayK)=t13(DisplayK)+1;
+               break;
+           elseif(Ch_data(k) == 2  && k~=FiredChannel)
+                GreatThan2(k) = GreatThan2(k) + 1;
+                t2=t2+1;
+                 t12(DisplayK)=t12(DisplayK)+1; 
+                 DisplayK ;
+                break;
+           elseif(Ch_data(k) == 1  && k~=FiredChannel)
+                GreatThan1(k) = GreatThan1(k) + 1; 
+                t1=t1+1;
+              %  t1
+                 t11(DisplayK)=t11(DisplayK)+1;
+               
+              break; 
+           end
+         end
         end
-        for k = 1:1:64
-            if(k == FiredChannel)
-                contunue;
-            elseif( k ==) 
-                
-            end
-        
-        end
-        FiredCount = FiredCount + 1;
-        SumData = SumData + Ch_data;
+      %end
+          FiredCount = FiredCount + 1;  
+
+        SumData =GreatThan3 + GreatThan2 + GreatThan1  ;
     end
 
-%     %----- Plot The Data -----%
-%     X = 1:9;
-%     Y = 1:9;
-%     C = zeros(9,9);
-%     for i = 1:9
-%         for j = 1:9        
-%             if (i == 9) || (j ==9)
-%                 C(i,j) = 0;
-%             else
-%                 C(i,j) = NewChannelData((i - 1)*8 + j);
-%             end
-%         end
-%     end
-%     
-%     figure(DisplayK);
-%     fig = pcolor(X,Y,C);
-%     % coclormap summer
-%     ax = gca;
-%     colormap(flipud(gray(4)))
-%     % a = axes;
-%     caxis([0,4]);
-%     colorbar('Ticks',[0.5,1.5,2.5,3.5],'TickLabels',{'<50','50~150','>150','Not Use'});
-%     axis ij
-%     axis square
-% 
-%     set(fig,'linestyle','-','edgecolor','b')
-% 
-%     legend_str = sprintf('The Pad that hitted \n header = %X, BCID = %u',header,BCID);
-%     h = legend(legend_str);
-%     set(h,'Location','northoutside');
-%     for i = 1:8
-%         for j = 1:8
-%             PadNum = j + 8*(i - 1);
-%             str = ['Ch', int2str(PadNum)];
-%             xText_o = 1.1;
-%             yText_o = 1.5;
-%             xText = xText_o + (j - 1);
-%             yText = yText_o + (i - 1);
-%             text('String',str,'Position',[xText, yText],'FontSize',10,'Color','r');
-%         end
-%     end
-end
+ SumData(FiredChannel)=FiredCount;
+        SumData;
+end    
+t1 
+t2
+t3
+%fid2=fopen('K:\dhcal\picture\A23\data\2.txt','w');fprintf(fid2,'%d\n',t12 );  fclose(fid2); 
+FiredCount
+FiredChannel
 
 %*** Plot Sum Data 2D
 NewSumData = SingleMapping(SumData, ASIC_Channel, Pad_Channel);
@@ -234,71 +210,3 @@ StrGreatThan3 = sprintf('Hitted Count: Charge Greater Than 150fC.(Total count:%d
 title(StrGreatThan3);
 colormap(flipud(parula))
 colorbar;
-for k = 1:length(b3)
-    zdata = b3(k).ZData;
-    b3(k).CData = zdata;
-    b3(k).FaceColor = 'interp';
-end
-
-WeightSum = (3*NewGreatThan1 + 20*NewGreatThan2 + 150*NewGreatThan3)/FiredCount;
-C3DWeight = zeros(8,8);   
-for i = 1:8
-    for j = 1:8        
-        C3DWeight(i,j) = (WeightSum((i - 1)*8 + j));
-    end
-end
-width = 1;
-figure;
-plot(WeightSum);
-ax = gca;
-ax.YScale = 'log';
-figure;
-bWeight = bar3(C3DWeight,width);
-set(gca,'ZScale','log')
-title('Average hitted charge')
-colormap(flipud(parula))
-colorbar;
-for k = 1:length(bWeight)
-    zdata = bWeight(k).ZData;
-    bWeight(k).CData = zdata;
-    bWeight(k).FaceColor = 'interp';
-end
-CWeight = zeros(9,9);
-for i = 1:9
-        for j = 1:9        
-            if (i == 9) || (j ==9)
-                CWeight(i,j) = 0;
-            else
-                CWeight(i,j) = WeightSum((i - 1)*8 + j);
-            end
-        end
-end
-figure;
-fig = pcolor(X,Y,CWeight);
-title('Average hitted charge')
-    % coclormap summer
-ax = gca;
-colormap(flipud(gray))
-colorbar;
-axis ij;
-axis square;
-legend_str = sprintf('The Pad that hitted \n header = %X, BCID = %u',header,BCID);
-    h = legend(legend_str);
-    set(h,'Location','northoutside');
-    for i = 1:8
-        for j = 1:8
-            PadNum = j + 8*(i - 1);
-            str = ['Ch', int2str(PadNum)];
-            xText_o = 1.1;
-            yText_o = 1.5;
-            xText = xText_o + (j - 1);
-            yText = yText_o + (i - 1);
-            text('String',str,'Position',[xText, yText],'FontSize',10,'Color','r');
-        end
-    end
-% ax = gca;
-% ax.Color = 'b';
-% ax.XAxis.Color = 'r';
-% ax.YAxis.Color = 'g';
-%h = axes('xcolor','y','ycolor','r','xgrid','on','ygrid','on');
-%set(h,);
