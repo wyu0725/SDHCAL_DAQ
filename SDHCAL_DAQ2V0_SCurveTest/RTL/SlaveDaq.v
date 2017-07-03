97faed8c5ffe82909e96e57a3bef4572622a4a9f
Yu Wang <wyu0831@hotmail.com>
2017-07-03 08:00:42 +0800

    将计数器输出与主时钟同步、

--- SDHCAL_DAQ2V0_SCurveTest/RTL/SlaveDaq.v
+++ SDHCAL_DAQ2V0_SCurveTest/RTL/SlaveDaq.v
@@ -325,6 +325,14 @@ module SlaveDaq(
       else
         TrigCounter <= TrigCounter;
     end
+    //*** Synchronise
+    reg [23:0] TrigCounter_sync;
+    always @(posedge Clk or negedge reset_n) begin
+      if(~reset_n)
+        TrigCounter_sync <= 24'b0;
+      else
+        TrigCounter_sync <= TrigCounter;
+    end
     //*** Power On Control
     always @(State) begin
       if(State == POWER_ON || State == POWER_ON || State == RELEASE || State == WAIT_START || State == START_ACQUISITION || State == WAIT_READ || State == START_READOUT || State == WAIT_READ || State == WAIT_READ_DONE || State == ONCE_END)
@@ -349,11 +357,11 @@ module SlaveDaq(
         SlaveDaqData_en <= InternalData_en;
       end
       else if(State == OUT_COUNT1) begin
-        SlaveDaqData <= {8'hCC,TrigCounter[23:16]};
+        SlaveDaqData <= {8'hCC,TrigCounter_sync[23:16]};
         SlaveDaqData_en <= InternalData_en;
       end
       else if(State == OUT_COUNT2) begin
-        SlaveDaqData <= TrigCounter[15:0];
+        SlaveDaqData <= TrigCounter_sync[15:0];
         SlaveDaqData_en <= InternalData_en;
       end
       else begin
--- SDHCAL_DAQ2V0_SCurveTest/SDHCAL_DAQ2V0.runs/synth_1/runme.sh
+++ SDHCAL_DAQ2V0_SCurveTest/SDHCAL_DAQ2V0.runs/synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=D:/Xilinx/SDK/2016.3/bin;D:/Xilinx/Vivado/2016.3/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2016.3/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2016.3/bin
+  PATH=D:/Xilinx/Vivado/2016.3/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2016.3/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2016.3/bin
 else
-  PATH=D:/Xilinx/SDK/2016.3/bin;D:/Xilinx/Vivado/2016.3/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2016.3/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2016.3/bin:$PATH
+  PATH=D:/Xilinx/Vivado/2016.3/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2016.3/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2016.3/bin:$PATH
 fi
 export PATH
 
