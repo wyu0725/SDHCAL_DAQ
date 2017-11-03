3f2d768c9d00ed4da7b25fe231e2e53fc5117672
Yu Wang <wyu0831@hotmail.com>
2017-11-03 17:36:38 +0800

    Microroc刻度

--- Matlab/SCurveTest/CalibrationMicroroc.m
+++ Matlab/SCurveTest/CalibrationMicroroc.m
@@ -1,4 +1,4 @@
-%% Calibration the ASIC
+%%% Calibration the ASIC
 Channel = 1:1:64;
 promptDataNumber = {'Input the start charge','Input the end charge', 'Input the charge interval'};
 dlgDataNumber = 'Input data info';
@@ -9,5 +9,21 @@ ChargeInterval = str2double(answer(3));
 DataNumber = (EndCharge - StartCharge)/ChargeInterval + 1;
 CurrentPath = pwd;
 for i = 1:1:DataNumber
-    
+    % Read Data Back
+    filename = sprintf('%s\\64_Chn_%dfC_CTest_DC.dat',CurrentPath,(i-1)*20);
+    [fid,~] = fopen(filename,'r');
+    if fid <= 0
+        % There was an error--tell user
+        str = ['File ' filename ' could not be opened.'];
+        dlg_title = 'File Open Faild';
+        errordlg(str, dlg_title,'modal');
+    else
+        %File opend successfully
+        InitialData = fread(fid,'uint16','ieee-be');%Big-endian ording
+        %Size = length(importdata);
+        fclose(fid);%close file
+    end
+    for j = 0:1:63
+        [DAC0_50Percent(j+1), DAC1_50Percent(j+1), DAC2_50Percent(j+1)] = SingleChannelTrigEfficiency(ImportData, j);
+    end
 end
