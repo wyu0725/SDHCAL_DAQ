64f10d3dd82af675af55c3fcd3f982b13f540c57
Yu Wang <wyu0831@hotmail.com>
2018-03-05 20:04:25 +0800

    添加MICROROCControl类
    使用MicrorocControl类来控制Microroc芯片配置，每个MicrorocControl实例控制一串Microroc

---
+++ SDHCAL_DAQ_USB_latest/USB_DAQ/MicrorocControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USB_DAQ
+{
+    class MicrorocControl
+    {
+        public MicrorocControl( int asicID)
+        {
+            AsicID = asicID;
+        }
+        public int AsicID { set; get; }
+    }
+}
