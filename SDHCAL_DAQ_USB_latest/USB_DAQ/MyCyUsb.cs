ceed31b4870e07d41fb110638fd03e61d43506a3
Yu Wang <wyu0831@hotmail.com>
2018-03-06 20:33:52 +0800

    使用USB类来实例化USB

--- SDHCAL_DAQ_USB_latest/USB_DAQ/MyCyUsb.cs
+++ SDHCAL_DAQ_USB_latest/USB_DAQ/MyCyUsb.cs
@@ -9,6 +9,12 @@ namespace USB_DAQ
 {
     class MyCyUsb
     {
+        /// <summary>
+        /// Create an instance of CyUSB. Utilize PID and VID to 
+        /// distinguish different cy devices
+        /// </summary>
+        /// <param name="pid"></param>
+        /// <param name="vid"></param>
         public MyCyUsb(int pid, int vid)
         {
             PID = pid;
@@ -20,13 +26,67 @@ namespace USB_DAQ
         private CyUSBDevice myDevice;
         private CyBulkEndPoint BulkInEndPt;
         private CyBulkEndPoint BulkOutEndPt;
+        /// <summary>
+        /// Initial device when new usb device is attached or detached
+        /// </summary>
+        /// <returns>True for attached & false for detached</returns>
         public bool InitialDevice()
         {
             myDevice = usbDevices[VID, PID] as CyUSBDevice;
-            if(myDevice == )
+            if(myDevice != null)
+            {
+                BulkInEndPt = myDevice.EndPointOf(0x86) as CyBulkEndPoint;
+                BulkInEndPt.XferSize = BulkInEndPt.MaxPktSize * 8;
+                BulkInEndPt.TimeOut = 10;
+                BulkOutEndPt = myDevice.EndPointOf(0x02) as CyBulkEndPoint;
+                return true;
+            }
+            else
+            {
+                BulkInEndPt = null;
+                BulkOutEndPt = null;
+                return false;
+            }            
         }
-
-        
-        
+        public byte[] ConstCommandByteArray(params byte[] paramList)
+        {
+            byte[] buffer = new byte[paramList.Length];
+            Array.Copy(paramList, buffer, paramList.Length);
+            byte temp;
+            for (int i = 0; i < buffer.Length; i += 2)
+            {
+                temp = buffer[i];
+                buffer[i] = buffer[i + 1];
+                buffer[i + 1] = temp;
+            }
+            return buffer;
+        }
+        public bool CommandSend(byte[] OutData, int xferLen)
+        {
+            bool bResult = false;
+            if (BulkInEndPt == null)
+            {
+                bResult = false;
+            }
+            else
+            {
+                bResult = BulkOutEndPt.XferData(ref OutData, ref xferLen);
+            }
+            return bResult;
+        }
+        public bool DataRecieve(byte[] InData, int xferLen)
+        {
+            bool bResult = false;
+            if(myDevice != null)
+            {
+                bResult = BulkInEndPt.XferData(ref InData, ref xferLen, true);
+            }
+            else
+            {
+                bResult = false;
+            }
+            return bResult;
+        }
+    
     }
 }
