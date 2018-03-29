9f1c2eabc6f0ba4b9184088a4594c060d0989676
Yu Wang <wyu0725@mail.ustc.edu.cn>
2018-03-29 08:56:42 +0800

    刻度ADC代码

---
+++ Matlab/ADC/CaculateAdcAutoImport.m
@@ -0,0 +1,36 @@
+function [ Average,Std ] = CaculateAdcAutoImport(AverageNumber, HistNumber, PathName, Charge)
+%CaculateAdc AverageNumber是多少个点将ADC的数据平均一次 HistNumber是为调用hist
+%   此处显示详细说明
+    filename = sprintf('%s\\%s\\%dfC.dat',pwd,PathName,Charge);
+    [fid,~] = fopen(filename,'r');
+    if fid <= 0
+        % There was an error--tell user
+        str = ['File ' filename ' could not be opened.'];
+        dlg_title = 'File Open Faild';
+        errordlg(str, dlg_title,'modal');
+    else
+        %File opend successfully
+        InitialData = fread(fid,'ubit16','ieee-be');%Big-endian ording
+        %Size = length(importdata);
+        fclose(fid);%close file
+    end
+    HitNumber = floor(length(InitialData)/AverageNumber);
+    AdcData = zeros(HitNumber,1);
+    for i = 1:1:HitNumber
+        SumAdcData = 0;
+        for j = 1:1:AverageNumber
+            DataIndex = (i - 1)*AverageNumber + j;
+            SumAdcData = SumAdcData + InitialData(DataIndex);
+        end
+        AdcData(i) = SumAdcData / AverageNumber;
+    end
+    
+    [AdcCount,Adc] = hist(AdcData,HistNumber);
+    h = histogram(AdcData,10);
+    hold on;
+    FAdc = AdcCount/sum(AdcCount);
+    Average = FAdc*Adc';
+    Std = sqrt(FAdc*((Adc-Average).*(Adc-Average))');
+
+
+end
