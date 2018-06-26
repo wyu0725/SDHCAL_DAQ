a7a1a613aa6e078c3b41fd8c3deb9e3b0e42fbd9
Yu Wang <wyu0725@mail.ustc.edu.cn>
2018-06-26 18:58:37 +0800

    Top层添加模块

--- SDHCAL_DIF1V0/src/FPGA_Top.v
+++ SDHCAL_DIF1V0/src/FPGA_Top.v
@@ -38,6 +38,34 @@ module FPGA_Top(
 	input Clk40M,
 	input rst_n
 	);
-	MicrorocControl MicrororChain1(
+	MicrorocControl MicrorocChain1(
 		);
+	MicrorocControl MicrorocChain2(
+	
+		);
+	/*-----------USB2.0 instantiation------------*/
+    wire [15:0] in_from_ext_fifo_dout;
+    wire out_to_ext_fifo_rd_en;
+    wire out_to_Microroc_SC_Param_Load;
+    wire UsbStartStop;
+    usb_synchronous_slavefifo usb_cy7c68013A
+    (
+      .IFCLK(IFCLK),
+      .FLAGA(FLAGA),
+      .FLAGB(FLAGB),
+      .FLAGC(FLAGC),
+      .nSLCS(nSLCS),
+      .nSLOE(nSLOE),
+      .nSLRD(nSLRD),
+      .nSLWR(nSLWR),
+      .nPKTEND(nPKTEND),
+      .FIFOADR(FIFOADR),
+      .FD_BUS(FD_BUS),
+      .Acq_Start_Stop(UsbStartStop),
+      .Ctr_rd_en(in_from_usb_Ctr_rd_en),              //fifo interface
+      .ControlWord(in_from_usb_ControlWord),          //fifo interface
+      .in_from_ext_fifo_dout(in_from_ext_fifo_dout),  //fifo interface
+      .in_from_ext_fifo_empty(in_from_ext_fifo_empty),//fifo interface
+      .out_to_ext_fifo_rd_en(out_to_ext_fifo_rd_en)   //fifo interface
+    );
 endmodule
