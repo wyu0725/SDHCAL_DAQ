ef2b42e6d0244105aef79e6dd555dcfa7205f33f
Yu Wang <wyu0725@mail.ustc.edu.cn>
2018-07-20 15:45:33 +0800

    TriggerSwitcher增加TriggerAnd, TriggerOr和TriggerExt输出
    所有的输出经过以后过一个BUFG驱动

--- SDHCAL_DIF1V0/src/TriggerSwitcher.v
+++ SDHCAL_DIF1V0/src/TriggerSwitcher.v
@@ -21,19 +21,63 @@
 
 
 module TriggerSwitcher(
-  input [4:0] TriggerSelect,
+  input SyncClk,
+  input reset_n,
+  input [3:0] TriggerSelect,
   input OutTrigger0b,
   input OutTrigger1b,
   input OutTrigger2b,
   input TriggerExt,
-  output Trigger
+  output Trigger,
+  output TriggerAnd,
+  output TriggerOr,
+  output ExternalTriggerSyncOut
     );
+  reg InternalTrigger0;
+  reg InternalTrigger1;
+  reg InternalTrigger2;
+  reg TriggerExternal;
+  always @ (posedge SyncClk or negedge reset_n) begin
+    if(~reset_n) begin
+      InternalTrigger0 <= 1'b0;
+      InternalTrigger1 <= 1'b0;
+      InternalTrigger2 <= 1'b0;
+      TriggerExternal <= 1'b0;
+    end
+    else begin
+      InternalTrigger0 <= ~OutTrigger0b;
+      InternalTrigger1 <= ~OutTrigger1b;
+      InternalTrigger2 <= ~OutTrigger2b;
+      TriggerExternal <= TriggerExt;
+    end
+  end
+  reg TriggerSelected;
   always @(*) begin
-    case(TriggerSwitcher)
-      4'b0000:Trigger = OutTrigger0b;
-      4'b0001:Trigger = OutTrigger1b;
-      4'b0010:Trigger = OutTrigger2b;
-      4'b1000:Trigger = TriggerExt;
+    case(TriggerSelect)
+      4'b0000:TriggerSelected = InternalTrigger0;
+      4'b0001:TriggerSelected = InternalTrigger1;
+      4'b0010:TriggerSelected = InternalTrigger2;
+      4'b1000:TriggerSelected = TriggerExternal;
     endcase
   end
+  BUFG BUFG_TRIGGER (
+      .O(Trigger), // 1-bit output: Clock output
+      .I(TriggerSelected)  // 1-bit input: Clock input
+   );
+  wire TriggerAndInternal;
+  assign TriggerAndInternal = InternalTrigger0 && InternalTrigger1 && InternalTrigger2;
+  BUFG BUFG_TRIGGER_AND (
+      .O(TriggerAnd), // 1-bit output: Clock output
+      .I(TriggerAndInternal)  // 1-bit input: Clock input
+   );
+  wire TriggerOrInternal;
+  assign TriggerOrInternal = InternalTrigger0 || InternalTrigger1 || InternalTrigger2;
+  BUFG BUFG_TRIGGER_OR (
+      .O(TriggerOr), // 1-bit output: Clock output
+      .I(TriggerOrInternal)  // 1-bit input: Clock input
+   );
+  BUFG BUFG_TRIGGER_EXT (
+      .O(ExternalTriggerSyncOut), // 1-bit output: Clock output
+      .I(TriggerExternal)  // 1-bit input: Clock input
+   );
 endmodule
