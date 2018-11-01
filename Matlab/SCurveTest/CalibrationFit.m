7880f46c6c7aa16f8de795279b09dfbdb86bd334
Yu Wang <wyu0725@mail.ustc.edu.cn>
2018-11-01 15:12:43 +0800

    根据刻度数据计算平均的增益

--- Matlab/SCurveTest/CalibrationFit.m
+++ Matlab/SCurveTest/CalibrationFit.m
@@ -15,6 +15,55 @@ for i = 1:1:64
     end
 end
 
-[FitSlopeDac0,FitInterceptDac0,FitRDac0] = SingleShaperFitInSingleAsic(ChargeHigh,Dac0Code);
-[FitSlopeDac1,FitInterceptDac1,FitRDac1] = SingleShaperFitInSingleAsic(ChargeHigh,Dac1Code);
-[FitSlopeDac2,FitInterceptDac2,FitRDac2] = SingleShaperFitInSingleAsic(ChargeLow,Dac2Code);
+figure;
+for i = 1:1:16
+    stairs(Dac0Code(:,i));
+    hold on;
+end
+figure;
+for i = 1:1:16
+    stairs(Dac1Code(:,i));
+    hold on;
+end
+figure;
+for i = 1:1:10
+    stairs(Dac2Code(:,i));
+    hold on;
+end
+figure;
+for i = 1:1:10
+    stairs(Dac0Code(:,i));
+    hold on;
+end
+
+[p0,R0] = ThresholdCalibrationLinearfit(ChargeHigh,Dac0Code);
+[p1,R1] = ThresholdCalibrationLinearfit(ChargeHigh,Dac1Code);
+[p2,R2] = ThresholdCalibrationLinearfit(ChargeLow,Dac2Code);
+p0Value = p0;
+p1Value = p1;
+p2Value = p2;
+k = 1;
+for i = 1:1:64
+    if( i == 55 ...,
+        || i == 56 ...,
+        || i == 60)
+    p0Value(k,:) = [];
+    p1Value(k,:) = [];
+        continue;
+    end
+    k = k+1;
+end
+k = 1;
+for i = 1:1:64
+    if( i == 55 ...,
+        || i == 56 ...,
+        || i == 28 ...,
+        || i == 60)
+    p2Value(k,:) = [];
+        continue;
+    end
+    k = k+1;
+end
+MeanP0 = mean(p0Value);
+MeanP1 = mean(p1Value);
+MeanP2 = mean(p2Value);
