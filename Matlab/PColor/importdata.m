e73c398feae5dabedca9dce2adadd24a3405f652
Yu Wang <wyu0725@mail.ustc.edu.cn>
2019-01-18 11:10:08 +0800

    如果文件选择错误，导入的数据设为0

--- Matlab/PColor/importdata.m
+++ Matlab/PColor/importdata.m
@@ -1,18 +1,21 @@
 function InitialData = ImportData()
-[FileName,PathName,FilterIndex] = uigetfile('*.dat','Select the file');
-if FilterIndex
-    filename = [PathName FileName];
-    [fid,~] = fopen(filename,'r');
-    if fid <= 0
-        % There was an error--tell user
-        str = ['File ' filename ' could not be opened.'];
-        dlg_title = 'File Open Faild';
-        errordlg(str, dlg_title,'modal');
+    [FileName,PathName,FilterIndex] = uigetfile('*.dat','Select the file');
+    if FilterIndex
+        filename = [PathName FileName];
+        [fid,~] = fopen(filename,'r');
+        if fid <= 0
+            % There was an error--tell user
+            str = ['File ' filename ' could not be opened.'];
+            InitialData = 0;
+            dlg_title = 'File Open Faild';
+            errordlg(str, dlg_title,'modal');
+        else
+            %File opend successfully
+            InitialData = fread(fid,'ubit2','ieee-be');%Big-endian ording
+            %Size = length(importdata);
+            fclose(fid);%close file
+        end
     else
-        %File opend successfully
-        InitialData = fread(fid,'ubit2','ieee-be');%Big-endian ording
-        %Size = length(importdata);
-        fclose(fid);%close file
+        InitialData = 0;
     end
-end
 end
